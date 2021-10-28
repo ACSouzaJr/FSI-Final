fd19bd1668b1f0a496e84a30d2d029d3b6f36221
antoniojunior <junioracs2000@gmail.com>
2021-10-28 16:43:02 -0300

    Project Start
    Co-authored-by: mayarachew <mayarachew@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FSI-Final
+
+## Rodar o projeto
+
+Instalar as bibliotecas : `pip install -r requirements`
+Run: `streamlit run news_classifier.py`
+
+## Dataset
+
+ Link: https://www.kaggle.com/rmisra/news-category-dataset
---
+++ hello_world.py
@@ -0,0 +1,89 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import time
+
+st.write("Here's our first attempt at using data to create a table:")
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+""" for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1) """
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+## Widgets
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+## Layout
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+import matplotlib.pyplot as plt
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
